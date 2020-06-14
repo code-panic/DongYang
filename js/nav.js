f004603517a5238b38567851c6f7672fb8eec8ee
ParkHyunSu <32615067+phs1234@users.noreply.github.com>
2020-06-15 03:23:43 +0900

    nav 무브기능 추가

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,14 @@
-window.onscroll = function() {
-    let nav = document.getElementsByTagName("nav")[0];
+const nav = document.getElementsByTagName("nav")[0];
+const nav_link_list = document.getElementById("nav_links").children;
 
+
+Array.from(nav_link_list).forEach(nav_link => {
+    nav_link.addEventListener("click", function() {
+        clickNavLink(nav_link)
+    });
+});
+
+window.onscroll = function() {
     if (window.scrollY > nav.offsetHeight) {
         nav.classList.add("nav_scrolled");
     } else {
@@ -11,3 +19,14 @@ window.onscroll = function() {
     // console.log("scroll: " + nav.offsetHeight);
 }
 
+function clickNavLink(nav_link) {
+    Array.from(nav_link_list).forEach(nav_link => {
+        nav_link.classList.remove("nav_active")
+    });
+
+    nav_link.classList.add("nav_active");
+
+    // console.log(nav_link.dataset.id);
+
+    nav_link.dataset.id
+}
