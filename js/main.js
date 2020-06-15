07d006256e8398c924b167d037e3a41bcd31e925
ParkHyunSu <32615067+phs1234@users.noreply.github.com>
2020-06-15 13:47:49 +0900

    메인 비디오 추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-window.onload = function() {
+let isPlaying = false;
 
-    
-}
+const main_video = document.getElementById("main_video");
+const main_bg = document.getElementById("main_bg");
+
+const main_logo = document.getElementById("main_logo");
+const main_desc_wrapper = document.getElementById("main_desc_wrapper");
+
+setInterval(function() {
+    if (window.scrollY < nav.offsetHeight) {
+        main_video.play();
+        main_bg.style.opacity = 0;
+        isPlaying = true; 
+    } else {
+        main_video.pause();
+        main_bg.style.opacity = 100;
+        isPlaying = false;
+    }
+}, 5000);
+
+// setInterval(function() {
+//     if (isPlaying) {
+//         main_logo.style.transform = "scale(0.6,0.6)";
+//     }
+// }, 10000);
