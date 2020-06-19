8f8112143f4bce4657e102a2b6b7f4f217e7008f
ParkHyunSu <32615067+phs1234@users.noreply.github.com>
2020-06-20 01:18:19 +0900

    캠퍼스 기능 테스트

--- js/campus.js
+++ js/campus.js
@@ -1,122 +1,62 @@
+
+// 빈 코드에 캔버스를 추가하는 방식
 // const campus_map_wrapper = document.getElementById("campus_map_wrapper");
 
-// const campus_building_9 = document.getElementById("campus_building_9");
-// const campus_building_9_canvas = document.createElement('canvas');
+// addCanvas('./img/campus_building_9.png');
+// // addCanvas('./img/campus_floor.png');
+// // addCanvas('./img/campus_building_8.png');
+// // addCanvas('./img/campus_building_7.png');
+// // addCanvas('./img/campus_building_6.png');
+// // addCanvas('./img/campus_building_5.png');
+// // addCanvas('./img/campus_building_4.png');
+// // addCanvas('./img/campus_building_3.png');
+// // addCanvas('./img/campus_building_2.png');
+// // addCanvas('./img/campus_building_1.png');
 
-// campus_building_9_canvas.width = campus_building_9.width;
-// campus_building_9_canvas.height = campus_building_9.height;
-// campus_building_9_canvas.getContext('2d').drawImage(campus_building_9, 0, 0, campus_building_9.width, campus_building_9.height);
+// function addCanvas(src) {
+//     const canvas = document.createElement("canvas");
+//     canvas.width = 800;
+//     canvas.height = 448;
 
-// const pixel = campus_building_9_canvas.getContext('2d').getImageData(1, 1, 1, 1).data;
+//     campus_map_wrapper.appendChild(canvas);
 
-// console.log("r : " + pixel[0] +" g : " + pixel[1] + " b : " + pixel[2] + " a : " + pixel[3]);
+//     const tmp_image = new Image();
+//     tmp_image.src = src;
 
-// campus_map_wrapper.addEventListener('mousemove', function(event) {
-//     const campus_map_wrapper_rect = campus_map_wrapper.getBoundingClientRect();
-
-//     // console.log("X : " + (event.clientX - campus_map_wrapper_rect.left) 
-//     //             + " Y : " + (event.clientY - campus_map_wrapper_rect.top));
-
-//     // const imgX = event.clientX - campus_map_wrapper_rect.left;
-//     // const imgY = event.clientY - campus_map_wrapper_rect.top;
-
-//     const elements = document.elementsFromPoint(event.clientX, event.clientY);
-
-//     // Array.from(elements).forEach(element => function() {
-//     //     if(element.tagName == "img" && )
-//     // });
-
-//     // console.log(elements);
-// });
-
-const campus_map_wrapper = document.getElementById("campus_map_wrapper");
-
-addCanvas('./img/campus_building_9.png');
-// addCanvas('./img/campus_floor.png');
-// addCanvas('./img/campus_building_8.png');
-// addCanvas('./img/campus_building_7.png');
-// addCanvas('./img/campus_building_6.png');
-// addCanvas('./img/campus_building_5.png');
-// addCanvas('./img/campus_building_4.png');
-// addCanvas('./img/campus_building_3.png');
-// addCanvas('./img/campus_building_2.png');
-// addCanvas('./img/campus_building_1.png');
-
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.fillStyle = "red";
-ctx.fillRect(10, 10, 50, 50);
-
-c.addEventListener('mousemove', function(event) {
-    const elements = document.elementsFromPoint(event.clientX, event.clientY);
-
-    Array.from(elements).forEach(element => {
-        if(element.tagName == "CANVAS") {
-            const pixel = element.getContext('2d').getImageData(event.clientX, event.clientY, 1, 1);
-            console.log(pixel.data);
-        }
-    });
-});
-
-
-function addCanvas(src) {
-    const canvas = document.createElement("canvas");
-    canvas.width = 800;
-    canvas.height = 448;
-
-    campus_map_wrapper.appendChild(canvas);
-
-    const tmp_image = new Image();
-    tmp_image.src = src;
-
-    canvas.setAttribute('data-id', src);
-
-    tmp_image.onload = function() {
-        canvas.getContext('2d').drawImage(tmp_image, 0, 0, canvas.width, canvas.height);
-        console.log("tmp_imgae is loaded");    
-        // tmp_image.setAttribute('crossOrigin', '');
-    }
-}
+//     tmp_image.onload = function() {
+//         canvas.getContext('2d').drawImage(tmp_image, 0, 0, canvas.width, canvas.height);
+//         console.log("tmp_imgae is loaded");    
+//         // tmp_image.setAttribute('crossOrigin', '');
+//     }
+// }
 
 // campus_map_wrapper.addEventListener('mousemove', function(event) {
-//     // const campus_map_wrapper_rect = campus_map_wrapper.getBoundingClientRect();
-
 //     const elements = document.elementsFromPoint(event.clientX, event.clientY);
 
 //     Array.from(elements).forEach(element => {
 //         if(element.tagName == "CANVAS") {
-//             // console.log(element);
-
-//             // const pixel = element.getContext('2d')
-//             //                 .getImageData(event.clientX - campus_map_wrapper_rect.left, 
-//             //                                 event.clientY - campus_map_wrapper_rect.top, 1, 1);
-
-//             // const pixel = element.getContext('2d').getImageData(event.clientX, event.clientY, 1, 1);
 //             const pixel = element.getContext('2d').getImageData(event.clientX, event.clientY, 1, 1);
-
 //             console.log(pixel.data);
-
-//             //console.log(element + "clientX : " + event.clientX + " clientY : " + event.clientY + " pixel data : " + pixel.data);
-//             // console.log(pixel.data[3]);
-        
-//             // if (pixel[3] != 0) {
-//             //     console.log(element.src);
-//             // }
 //         }
 //     });
-
-//     // console.log(elements);
 // });
 
-campus_map_wrapper.addEventListener('mousemove', function(event) {
-    const elements = document.elementsFromPoint(event.clientX, event.clientY);
+// 이미지 태그 곁으로 안 보이는 canvas 태그를 쓰는 방식
 
-    Array.from(elements).forEach(element => {
-        if(element.tagName == "CANVAS") {
-            const pixel = element.getContext('2d').getImageData(event.clientX, event.clientY, 1, 1);
-            console.log(pixel.data);
-        }
-    });
+const image = document.getElementById('image');
+const canvas = document.createElement('canvas');
+canvas.width = 800;
+canvas.height = 448;
+
+const tmp_image = new Image();
+tmp_image.src = "./img/campus_building_8.png";
+
+tmp_image.onload = function() {
+    canvas.getContext('2d').drawImage(tmp_image, 0, 0, canvas.width, canvas.height);
+}
+
+image.addEventListener('mousemove', function(event) {
+    var pixelData = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
+    console.log(pixelData);
 });
-
 
