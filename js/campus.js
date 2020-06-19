a6d6e75a97fa7add7e86c4e1615432076dc48044
ParkHyunSu <32615067+phs1234@users.noreply.github.com>
2020-06-20 01:33:40 +0900

    캠퍼스 기능 테스트

--- js/campus.js
+++ js/campus.js
@@ -41,9 +41,28 @@
 //     });
 // });
 
-// 이미지 태그 곁으로 안 보이는 canvas 태그를 쓰는 방식
+// 이미지 태그 곁으로 안 보이는 canvas 태그를 쓰는 방식 -> 성공
 
-const image = document.getElementById('image');
+// const image = document.getElementById('image');
+// const canvas = document.createElement('canvas');
+// canvas.width = 800;
+// canvas.height = 448;
+
+// const tmp_image = new Image();
+// tmp_image.src = "./img/campus_building_8.png";
+
+// tmp_image.onload = function() {
+//     canvas.getContext('2d').drawImage(tmp_image, 0, 0, canvas.width, canvas.height);
+// }
+
+// image.addEventListener('mousemove', function(event) {
+//     var pixelData = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
+//     console.log(pixelData);
+// });
+
+// 빈 코드에 캔버스를 추가하는 방식
+const campus_map_wrapper = document.getElementById("campus_map_wrapper");
+
 const canvas = document.createElement('canvas');
 canvas.width = 800;
 canvas.height = 448;
@@ -55,8 +74,11 @@ tmp_image.onload = function() {
     canvas.getContext('2d').drawImage(tmp_image, 0, 0, canvas.width, canvas.height);
 }
 
-image.addEventListener('mousemove', function(event) {
+campus_map_wrapper.appendChild(canvas);
+
+campus_map_wrapper.addEventListener('mousemove', function(event) {
     var pixelData = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
     console.log(pixelData);
 });
+
 
