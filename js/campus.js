9e0295cb52645d4f06cd7fa40908eb4b815feb89
ParkHyunSu <32615067+phs1234@users.noreply.github.com>
2020-06-21 03:54:01 +0900

    캠퍼스 초기세팅 변경

--- js/campus.js
+++ js/campus.js
@@ -39,6 +39,10 @@ function addCanvasImage(src) {
     //data-id 설정 코드 
     canvas.setAttribute('data-id', src.split("/")[2].split(".")[0]);
 
+    if(canvas.dataset.id == "canvas_building_1") {
+        canvas.classList.add('clicked');
+    }
+
     const tmp_image = new Image();
     tmp_image.src = src;
 
