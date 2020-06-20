68392d56655973dbfb771626bcc3f3bfeb7e58ee
ParkHyunSu <32615067+phs1234@users.noreply.github.com>
2020-06-20 18:10:13 +0900

    캠퍼스 기능 테스트

--- js/campus.js
+++ js/campus.js
@@ -97,10 +97,11 @@ campus_map_wrapper.onclick =  function(event) {
                         campus_building_table.innerHTML = "";
 
                         console.log(building['floors']);
+                        console.log(Array.isArray(building['floors']));
 
-                        Array.from(building['floors']).foreach(floor => {
-                            campus_building_table += '<tr><td class="floor">' +  floor['name'] + '</td>' +'<td>' + floor['desc'] + '</td></tr>';          
-                        });
+                        // Array.from(building['floors']).foreach(floor => {
+                        //     campus_building_table += '<tr><td class="floor">' +  floor['name'] + '</td>' +'<td>' + floor['desc'] + '</td></tr>';          
+                        // });
 
                         building['floors'].foreach(floor => {
                             campus_building_table += '<tr><td class="floor">' +  floor['name'] + '</td>' +'<td>' + floor['desc'] + '</td></tr>';          
